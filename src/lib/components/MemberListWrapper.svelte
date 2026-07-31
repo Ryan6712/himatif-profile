cd65b6b4f03ab5dfbeb4946d83bc0efd30ed3b96
Ryan6712 <brustface32@gmail.com>
2026-07-31 20:02:13 +0700

    fix some bugs

---
+++ src/lib/components/MemberListWrapper.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let { jabatan = "himatif" , children } = $props()
+
+
+</script>
+
+
+<div class="wrapper bg-amber-200">
+    <div class="title-wrapper flex flex-row gap-4 items-center pb-2 border-b-4 border-b-tertiary">
+        <img src="https://placehold.co/48x48/png?text=himatif" alt="">
+        <h1 class="text-xl font-bold">{jabatan}</h1>
+    </div>
+
+    <div class="content">
+        {@render children()}
+    </div>
+</div>
---
+++ src/routes/member/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import MemberListWrapper from "$lib/components/MemberListWrapper.svelte";
+
+
+</script>
+
+<div class="list-wrapper container">
+	<MemberListWrapper>
+		<div>
+			<h1>
+				disini list mamber
+			</h1>
+		</div>
+	</MemberListWrapper>
+</div>
