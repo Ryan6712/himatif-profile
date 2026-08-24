f40a05fe6080906f723fe47dd959f7190ee4bf39
Ryan6712 <brustface32@gmail.com>
2026-08-24 16:52:16 +0700

    I think it will fix login error

--- src/lib/authClient.ts
+++ src/lib/authClient.ts
@@ -1,7 +1,8 @@
+import { BETTER_AUTH_URL } from "$env/static/private";
 import { createAuthClient } from "better-auth/client";
 import { usernameClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
     plugins: [ usernameClient() ],
-    baseURL: "http://localhost:5173",
+    baseURL: BETTER_AUTH_URL,
 })
